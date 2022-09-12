6b46e1f4c54bec5252516c12af8c3339bf6a1753
Deku067 <73478007+mahdavia067@users.noreply.github.com>
2022-09-12 13:31:12 +0430

    init Project

---
+++ resources/views/admin/register.blade.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.rtl.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/register_style.css')}}">
+    <link rel="stylesheet" href="{{asset('node_modules/bootstrap-icons')}}">
+
+
+
+
+    <title>Document</title>
+</head>
+<body>
+
+
+    <div class="container">
+        <div class="row ">
+
+            <div class="form-group">
+
+                <form action="" method="post">
+
+                        <div style="margin: 7% auto 2% auto;width: 40% ;">
+                            <h2 style="margin-bottom: 5%;font-family: IRANSansXFaNum-Bold; font-size: 24px"><a href="#" style="color: #A044FF; ">ورود</a>/ثبت نام</h2>
+                                <input type="text" class="form-control" placeholder=" نام کاربری" style="font-family: IRANSansXFaNum-Light;font-size: 16px">
+                        </div>
+
+                        <div style="margin: 3% auto 2% auto;width: 40%">
+                            <input type="text" class="form-control " placeholder="موبایل" style="font-family: IRANSansXFaNum-Light;font-size: 16px">
+                        </div>
+
+                        <div style="margin: 3% auto 2% auto;width: 40%">
+                            <input type="text" class="form-control " placeholder="رمزعبور" style="font-family: IRANSansXFaNum-Light;font-size: 16px">
+                        </div>
+
+                    <div style="margin: 1% auto 6% auto;width: 40%">
+                        <div>
+                            <input type="checkbox" class="form-check-input" style="border: 1.5px solid #41444B; width: 18.5px; height: 18.5px">
+                            <label for="" class="px-2" style="font-family: IRANSansXFaNum-Medium;font-size: 14px">تمامی <a href="#" style="color: #A044FF;">شرایط و قوانین</a> را میپذیرم.</label>
+                        </div>
+
+                    </div>
+
+
+
+
+                        <div style="margin: 3% auto 4% auto;width: 40%">
+                            <button class="" style="
+                            background: linear-gradient(180deg, #6A3093 0%, #A044FF 100%);
+                            border:hidden;
+                            border-radius: 10px;
+                            color: #FFFFFF;
+                            font-family: IRANSansXFaNum-Medium;
+                            font-size: 16px">ثبت نام
+
+
+                            </button>
+                        </div>
+
+                    <div  style="margin: 1% auto 6% auto;width: 40% ;text-align: center">
+                        <div>
+                            <label for="" class="px-2" style="font-family: IRANSansXFaNum-Medium;font-size: 14px ; ">آیا قبلا ثبت نام کرده اید؟ <a href="#" style="color: #A044FF">ورود</a></label>
+                        </div>
+
+                    </div>
+
+                </form>
+
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+</html>
